259ef234ccc270c7b5d7f65aec7f912d7c29d8e7
JITSUZAKI Naoto <naoto.j3781@gmail.com>
2019-07-08 13:24:29 +0900

    Ver. 2.0.0

--- lap_time.py
+++ lap_time.py
@@ -9,7 +9,8 @@ def main():
         num_lane.append(input("{0}人目のレーン番号入力：".format(i+1)))
     for i in range(num_mem):
         mem_time[num_lane[i]] = []
-    while True:
+    dist = int(input("距離：")) // 50
+    for roop in range(dist*num_mem):
         print("登録済みレーン番号 => {0}".format(num_lane))
         in_lane = input("スプリットタイムを入力するレーンの番号を入力：")
         if in_lane in num_lane:
@@ -30,9 +31,6 @@ def main():
                 mem_time[in_lane].append(datetime.timedelta(minutes=min, seconds=sec, microseconds=micro_sec))
             else:
                 print("Input Error: 入力し直してください")
-            branch = input("終了する場合は0を入力、続けて入力する場合は1を入力：")
-            if branch == "0":
-                break
         else:
             print("そのレーン番号は登録されていません")
 
@@ -44,7 +42,26 @@ def main():
         print("第{0}レーン".format(in_lane))
         print(str(mem_time[in_lane][0]))
         for i in range(int(len(mem_time[in_lane])) - 1):
-            print(str(mem_time[in_lane][i+1]) + "({0})".format(lap[i]))
+            if i % 2 == 1:
+                print(str(mem_time[in_lane][i+1]) + "({0})".format(lap[i]))
+            elif  (i % 2 == 0) & (i == 0):
+                print(str(mem_time[in_lane][i+1]) + "({0}".format(lap[i]) + ", " + "{0})".format(mem_time[in_lane][0] + lap[i]))
+            else:
+                print(str(mem_time[in_lane][i+1]) + "({0}".format(lap[i]) + ", " + "{0})".format(lap[i-1] + lap[i]))
+
+    with open("./result.txt", mode="a") as f:
+        f.write("<<RESULT>>\n")
+        for in_lane in num_lane:
+            f.write("第{0}レーン\n".format(in_lane))
+            f.write(str(mem_time[in_lane][0]) + "\n")
+            for i in range(int(len(mem_time[in_lane])) - 1):
+                if i % 2 == 1:
+                    f.write(str(mem_time[in_lane][i+1]) + "({0})\n".format(lap[i]))
+                elif (i % 2 == 0) & (i == 0):
+                    f.write(str(mem_time[in_lane][i+1]) + "({0}".format(lap[i]) + ", " + "{0})\n".format(mem_time[in_lane][0] + lap[i]))
+                else:
+                    f.write(str(mem_time[in_lane][i+1]) + "({0}".format(lap[i]) + ", " + "{0})\n".format(lap[i-1] + lap[i]))
+        f.write("\n")
 
 if __name__ == "__main__":
     Flag = 1
